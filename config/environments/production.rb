4cf019d29f3b8f26e0b651ef14c0b31296f40438
Madhavi Nyros <madhavi_nyros@yahoo.com>
2012-04-11 09:34:18 +0530

    9:34pm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Device::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+config.assets.compile = true
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
